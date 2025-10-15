95239f42dd8618ad08846ce459d73a98f72a8b85
cuichaowen <cuichaowen@cambricon.com>
2025-10-15 14:40:29 +0800

    Works with the newest Triton codebase‌4

--- lib/Conversion/TritonToTritonGPU/TritonGPUConversion.cpp
+++ lib/Conversion/TritonToTritonGPU/TritonGPUConversion.cpp
@@ -13,7 +13,7 @@
 using namespace mlir;
 using namespace mlir::triton::gpu;
 
-RankedTensorType cloneWithEncoding(RankedTensorType type, ::mlir::Attribute encoding) {
+inline RankedTensorType cloneWithEncoding(RankedTensorType type, ::mlir::Attribute encoding) {
   return RankedTensorType::get(type.getShape(), type.getElementType(), encoding);
 }
 
